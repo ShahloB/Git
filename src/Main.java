a5a6828e1b37211715b58f77d7d1c8c651bc758c
ShahloB <bahodirovashahlo3@gmail.com>
2024-11-13 12:10:09 +0500

    message

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public void show(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
